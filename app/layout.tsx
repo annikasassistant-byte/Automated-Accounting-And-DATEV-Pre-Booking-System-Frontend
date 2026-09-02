adb4cd6d456c19e85cd3aa315ddf15a8556b2ce4
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2026-09-02 21:08:09 +0200

    new codessss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppProviders } from "@/providers/app-providers";
-// next
+// next logs
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
